ca8ea202efd8ccdfd4c3a0c30047c9767ede011f
mhasnain23 <tryfrelancer23@gmail.com>
2025-01-25 22:34:00 +0500

    updated

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,12 @@
+import { Pricing } from "@/components/Pricing";
+import React from "react";
+
+const PricingPage = () => {
+  return (
+    <div>
+      <Pricing />
+    </div>
+  );
+};
+
+export default PricingPage;
